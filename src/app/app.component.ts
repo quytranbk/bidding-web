82b89ffc11562badb2c4e5e134d75d3868b78a27
longnh18 <v.longnh18@vinid.net>
2019-11-26 11:01:09 +0700

    fixing menu list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Constants } from './constants'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bidding-web';
+  HEADER_MENU_LIST = Constants.HEADER_MENU;
+  constructor() {}
 }
---
+++ src/app/constants.ts
@@ -0,0 +1,34 @@
+export const Constants = {
+  HEADER_MENU: [
+    {
+      label: 'Trang chủ',
+      url: '',
+      isActive: true
+    },
+    {
+      label: 'Quần áo',
+      url: '',
+      isActive: false
+    },
+    {
+      label: 'Điện & điện tử',
+      url: '',
+      isActive: false
+    },
+    {
+      label: 'Xe cộ',
+      url: '',
+      isActive: false
+    },
+    {
+      label: 'Dụng cụ',
+      url: '',
+      isActive: false
+    },
+    {
+      label: 'thiết bị',
+      url: '',
+      isActive: false
+    },
+  ]
+}
